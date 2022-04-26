b746de346db291beae1ed59a909d0e5819ff741d
Wonder Jung <wonder13662@gmail.com>
2022-04-26 09:17:09 +0900

    [#3]인사말에 시간 정보를 추가

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const getHours = () => {
+  return new Date().getHours();
+};
+
 const enrichGreetingMessage = () => {
   // TODO 시간대별로 인사말이 달라야 합니다.
-  return 'Hello world!';
+  return `Hello world! at ${getHours()}`;
 };
 
 const greetings = () => {
