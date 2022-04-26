ce8ae851b8655dce935858dbffc24e12ce3c64b4
Wonder Jung <wonder13662@gmail.com>
2022-04-26 09:13:19 +0900

    [#2]인사말을 별도 메서드로 추출

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const enrichGreetingMessage = () => {
+  // TODO 시간대별로 인사말이 달라야 합니다.
+  return 'Hello world!';
+};
+
 const greetings = () => {
-  console.log('Hello world!');
+  console.log(enrichGreetingMessage());
 };
 
 greetings();
